f39d214bab0355ef2748325c5de9f60fe3c3048d
LinusBF <contact@linusbf.com>
2022-07-02 13:13:40 +0200

    Improved logger usage

--- index.js
+++ index.js
@@ -25,30 +25,38 @@ const VALID_HTTP_STATUS_CODES = Object.keys(http.STATUS_CODES).map((status) => {
  * @returns {(function(e.Request, e.Response))}
  */
 module.exports = function (logger = DEFAULT_LOGGER) {
+  const _logName = '@ExpressErrorHandler';
+  const _logger = {
+    debug: (msg, ...args) => logger.debug(`${_logName}: ${msg}`, ...args),
+    info: (msg, ...args) => logger.info(`${_logName}: ${msg}`, ...args),
+    warn: (msg, ...args) => logger.warn(`${_logName}: ${msg}`, ...args),
+    error: (msg, ...args) => logger.error(`${_logName}: ${msg}`, ...args),
+  };
+
   return function (req, res) {
-    logger.debug(`Got error on ${req.method} ${req.url}`);
+    _logger.debug(`Got error on ${req.method} ${req.url}`);
     const error = req?.error;
     const message =
       req?.error?.message || req?.error?.toString() || req?.message || 'Internal Server Error';
 
     if (typeof error === 'undefined') {
       //We have no error set on the request object, weird, I guess we just respond with a generic 500 error
-      logger.warn(`No error set on request object, responding with 500`);
-      logger.debug(`Req message: ${req?.message || req?.toString()}`);
+      _logger.warn(`No error set on request object, responding with 500`);
+      _logger.debug(`Req message: ${req?.message || req?.toString()}`);
       res.status(500).send(message);
       return;
     }
 
     if (!VALID_HTTP_STATUS_CODES.includes(error.status)) {
-      logger.warn(`Invalid error status code ${error.status}, responding with 500`);
-      logger.debug(`Error message: ${message}`);
+      _logger.warn(`Invalid error status code ${error.status}, responding with 500`);
+      _logger.debug(`Error message: ${message}`);
       // We might want to always send 'Internal Server Error' here for safety
       // since we don't know where the error came from
       res.status(500).send(message);
       return;
     }
 
-    logger.debug(`Responding with ${error.status} ${message}`);
+    _logger.debug(`Responding with ${error.status} ${message}`);
     res.status(error.status).send(message);
   };
 };
